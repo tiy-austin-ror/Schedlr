8ce09ec57d69263312e1f96ca84c17d875a2f450
Adam Ross <adamgarrettross@gmail.com>
2015-10-20 14:50:51 -0500

    added before filter to dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,7 @@
 class DashboardController < ApplicationController
+  before_filter :authenticate_user!
+  before_filter do
+    redirect_to root_path unless current_user && current_user.admin?
+  end
+
 end
