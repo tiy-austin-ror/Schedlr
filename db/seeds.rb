9797253382334dc3035735fe5b861fe8eca5f250
Ryan Yep <ryan.yep@gmail.com>
2015-10-20 14:11:20 -0500

    Staying in sync with master

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Company.create({
+
+      name: "Test Company" })
+
+      {
+        20.times do
+          User.create({
+                first_name:          ,
+                last_name:           ,
+                email:               ,
+                department:          ,
+                admin:          false
+            })
+        end
+      }
