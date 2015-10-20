ea524b326beb2d907188be48eab5c5e352119f44
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-10-20 16:52:13 -0500

    invite company.users button

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,7 +1,9 @@
 class InviteMailer < ApplicationMailer
   default from: 'schedlr@example.com'
 
-  def send_invite(user)
-    @user = user
+  def send_invite(invite)
+    @user = invite.user
+    @event = invite.event
+    @event_organizer = @event.user
     mail(to: @user.email, subject: "You have been invited to attend an event." )
 end
