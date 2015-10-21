50f6424871d32ffcbc9a95eb063d62f943343743
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-10-21 10:06:27 -0500

    Admin page shows invitees for current admin's company and change association in company model

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,6 +7,6 @@ class DashboardController < ApplicationController
   def index
     @rooms = current_user.company.rooms
     @events = current_user.company.events
-    @invitees = Invitee.all
+    @invitees = current_user.company.invitees
   end
 end
