22d19f18e2f00a61ee899a0573668853425c2207
Ryan Yep <ryan.yep@gmail.com>
2015-10-21 10:11:53 -0500

    Add changes per Senior dev's recs.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,13 @@ class Event < ActiveRecord::Base
   belongs_to :room
   belongs_to :user
   has_many :invitees
+
+  def formatted_start_time
+    self.start_time.strftime("%B %d, %Y @ %I:%M%P")
+  end
+
+  def formatted_event_duration
+    "#{self.duration} minutes"
+  end
+
 end
