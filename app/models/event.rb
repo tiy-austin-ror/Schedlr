7460970c9923b11f66592978d3ce12874c53b364
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-10-21 09:05:14 -0500

    Add button to add yourself to event page

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ActiveRecord::Base
   belongs_to :room
   belongs_to :user
   has_many :invitees
+  has_many :attendees
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :attendees
   resources :invitees
   devise_for :users
   devise_scope :user do
