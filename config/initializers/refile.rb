19a30f7c369b56ecf07989982c74b5c9715e898f
Adam Ross <adamgarrettross@gmail.com>
2015-10-22 09:18:04 -0500

    Edited the refile.rb

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
     access_key_id: ENV["access_key_id"],
     secret_acccess_key: ENV["secret_access_key"],
     region: "US Standard",
-    bucket: " pictures-of-rooms-from-garrett",
+    bucket: "pictures-of-rooms-from-garrett",
   }
   Refile.cache = Refile::S3.new(prefix: "cache", **aws)
   Refile.store = Refile::S3.new(prefix: "store", **aws)
