259d05433e41825cb2809bf01cc0284a7a63ae21
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-10-22 16:05:11 -0500

    search bar rooms index

--- app/assets/javascripts/components/search_rooms.js.jsx
+++ app/assets/javascripts/components/search_rooms.js.jsx
@@ -1,71 +1,66 @@
 var Search = React.createClass({
-    getDefaultProps: function () {
-      return {
-        rooms: [],
-      };
-    },
+  getInitialState: function () {
+    return {
+      filtered_rooms: [],
+      all_rooms: [],
+      search: ''
+    };
+  },
 
-    componentDidMount: function () {
-      $.ajax({
-        url: '/company/rooms',
-        dataType: 'JSON',
-        method: 'GET'
-      }).done(function (response) {
-        this.setState({
-          rooms: response
-        });
-      }.bind(this));
-    },
-
-    getInitialState: function () {
-      return {
-        rooms: this.props.rooms,
-        search: ''
-      };
-    },
-
-    handleChange: function (event) {
-      var matched_rooms = this.props.rooms.filter(function (room) {
-        return (room.name.toLowerCase().indexOf(event.target.value.toLowerCase()) > -1);
-      });
+  componentDidMount: function () {
+    $.ajax({
+      url: '/company/rooms',
+      dataType: 'JSON',
+      method: 'GET'
+    }).done(function (response) {
       this.setState({
-        search: event.target.value,
-        rooms: matched_rooms
+        all_rooms: response,
+        filtered_rooms: response
       });
-    },
+    }.bind(this));
+  },
 
-    render: function() {
-      var buildings = this.state.buildings;
-      return (
-        <section>
-          <p>
-            <input className='form-control' type='text'
-              onChange={this.handleChange} value={this.state.search}
-              placeholder='search rooms' />
-          </p>
-          <div className="table-responsive">
-            <table className="table table-hover">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Capacity</th>
-                  <th>Building name</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.rooms.map(function (room) {
-                  return (
-                    <tr>
-                      <td> { room.name } </td>
-                      <td> { room.capacity }</td>
-                      <td> { room.building.name }</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-        </section>
-    );
+  render: function() {
+    return (
+      <section>
+        <p>
+          <input className='form-control' type='text'
+            onChange={this.handleChange} value={this.state.search}
+            placeholder='search rooms' />
+        </p>
+        <div className="table-responsive">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Capacity</th>
+                <th>Building name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.filtered_rooms.map(function (room) {
+                return (
+                  <tr>
+                    <td> { room.name } </td>
+                    <td> { room.capacity }</td>
+                    <td> { room.building.name }</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </section>
+  );
+},
+
+  handleChange: function (event) {
+    var filtered_rooms = this.state.all_rooms.filter(function (room) {
+      return (room.name.toLowerCase().indexOf(event.target.value.toLowerCase()) > -1);
+    });
+    this.setState({
+      search: event.target.value,
+      filtered_rooms: filtered_rooms
+    });
   }
 });
