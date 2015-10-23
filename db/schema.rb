580c20b871ea02a819a50cb5d20bcbb89e41bb4e
Travis Montgomery <travisbm@gmail.com>
2015-10-22 22:42:05 -0500

    added attendee_count column to user table. added top 3 users list to admin page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023020158) do
+ActiveRecord::Schema.define(version: 20151023031724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,7 @@ ActiveRecord::Schema.define(version: 20151023020158) do
     t.datetime "updated_at",                             null: false
     t.integer  "company_id"
     t.boolean  "admin",                  default: false
+    t.integer  "attendees_count"
   end
 
   add_index "users", ["company_id"], name: "index_users_on_company_id", using: :btree
